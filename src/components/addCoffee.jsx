b0dd50edb0f25030de3a988c1ca033ab98104efc
anayetabir <anayetabir007@gmail.com>
2024-01-12 12:30:26 +0600

    Read Properties

--- src/components/addCoffee.jsx
+++ src/components/addCoffee.jsx
@@ -2,7 +2,23 @@
 
 const AddCoffee = () => {
 
-    
+    const handleAddCoffee = event => {
+        event.preventDefault();
+
+        const form = event.target;
+
+        const name = form.name.value;
+        const quantity = form.quantity.value;
+        const supplier = form.supplier.value;
+        const taste = form.taste.value;
+        const category = form.category.value;
+        const details = form.details.value;
+        const photo = form.photo.value;
+
+        const newCoffee = { name, quantity, supplier, taste, category, details, photo }
+        console.log(newCoffee);
+
+    }
 
 
     return (
@@ -10,7 +26,7 @@ const AddCoffee = () => {
 
             <h2 className="text-3xl font-extrabold" >Add Coffee</h2>
 
-            <form>
+            <form onSubmit={handleAddCoffee}>
                 {/* form name & quantity row */}
                 <div className="md:flex mb-8">
                     <div className="form-control md:w-1/2 ">
@@ -19,7 +35,7 @@ const AddCoffee = () => {
 
                         </label>
                         <label className="input-group">
-                            <input type="text" name="" placeholder="Coffee Name" className="input input-bordered w-full" />
+                            <input type="text" name="name" placeholder="Coffee Name" className="input input-bordered w-full" />
                         </label>
 
                     </div>
@@ -52,12 +68,12 @@ const AddCoffee = () => {
 
                         </label>
                         <label className="input-group">
-                            <input type="text" name="taste" placeholder="taste" className="input input-bordered w-full" />
+                            <input type="text" name="taste" placeholder="Taste" className="input input-bordered w-full" />
                         </label>
 
                     </div>
                 </div>
-                {/* form  category & details row  row */}
+                {/* form  category & details  row */}
                 <div className="md:flex mb-8">
                     <div className="form-control md:w-1/2 ">
                         <label className="label">
@@ -71,11 +87,11 @@ const AddCoffee = () => {
                     </div>
                     <div className="form-control md:w-1/2 ml-4">
                         <label className="label">
-                            <span className="label-text">Datails</span>
+                            <span className="label-text">Details</span>
 
                         </label>
                         <label className="input-group">
-                            <input type="text" name="datails" placeholder="Details" className="input input-bordered w-full" />
+                            <input type="text" name="details" placeholder="Details" className="input input-bordered w-full" />
                         </label>
 
                     </div>
